19d012d6cd69ab597b1ca0860db4345472c60988
배성훈 <dan990429@gmail.com>
2023-10-23 14:01:21 +0900

    tera as template engine

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
 #[macro_use]
 extern crate rocket;
+use rocket_dyn_templates::{context, Template};
 
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+fn index() -> Template {
+    Template::render("index", context!(title: "This is Title"))
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build()
+        .attach(Template::fairing())
+        .mount("/", routes![index])
 }
-
